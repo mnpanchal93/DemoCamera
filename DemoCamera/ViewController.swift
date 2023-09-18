1e7789abdbc22c3dac532e394cd29eadefb10576
Mansi <mnpanchal93@gmail.com>
2023-09-18 10:36:33 +0200

    Initial Commit

---
+++ DemoCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoCamera
+//
+//  Created by NFQ-222 on 18.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
